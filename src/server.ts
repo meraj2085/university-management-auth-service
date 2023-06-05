dd95baea3b51086203195d3736bbc04b8ad6ddee
meraj2085 <samirmeraj60@gmail.com>
2023-06-05 21:28:18 +0600

    Adding winston

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { error_logger, logger } from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`🛢️ Database is connected successfully`)
+    logger.info(`🛢️ Database is connected successfully`)
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}...`)
+      logger.info(`Application listening on port ${config.port}...`)
     })
   } catch (err) {
-    console.log('❌ Failed to connect database!', err)
+    error_logger.error('❌ Failed to connect database!', err)
   }
 }
 bootstrap()
