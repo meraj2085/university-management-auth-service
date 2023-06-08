0741c47179d500a1ec111f92d9ab64972831dc1e
meraj2085 <samirmeraj60@gmail.com>
2023-06-08 12:59:13 +0600

    Updaring zod

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,21 +1,21 @@
-import config from '../../../config/index'
-import ApiError from '../../../errors/ApiError'
-import { IUser } from './user.interface'
-import { User } from './user.model'
-import { generateUserId } from './user.utils'
+import config from '../../../config/index';
+import ApiError from '../../../errors/ApiError';
+import { IUser } from './user.interface';
+import { User } from './user.model';
+import { generateUserId } from './user.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-  const id = await generateUserId()
+  const id = await generateUserId();
 
-  user.id = id
+  user.id = id;
   if (!user.password) {
-    user.password = config.default_user_password as string
+    user.password = config.default_user_password as string;
   }
-  const createUser = await User.create(user)
-  if (!createUser) throw new ApiError(400, 'Failed to create user!')
-  return createUser
-}
+  const createUser = await User.create(user);
+  if (!createUser) throw new ApiError(400, 'Failed to create user!');
+  return createUser;
+};
 
 export const UserService = {
   createUser,
-}
+};
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,19 +1,19 @@
-import { createLogger, format, transports } from 'winston'
-const { combine, timestamp, printf } = format
-import DailyRotateFile from 'winston-daily-rotate-file'
-import path from 'path'
+import { createLogger, format, transports } from 'winston';
+const { combine, timestamp, printf } = format;
+import DailyRotateFile from 'winston-daily-rotate-file';
+import path from 'path';
 
 // Custom log format
 const myFormat = printf(({ level, message, timestamp }) => {
-  const date = new Date(timestamp)
-  const day = date.getDate()
-  const month = date.getMonth() + 1
-  const year = date.getFullYear()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
-  return `[ ${level}: ${message} [${day}/${month}/${year} | ${hours}:${minutes}:${seconds}]`
-})
+  const date = new Date(timestamp);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `[ ${level}: ${message} [${day}/${month}/${year} | ${hours}:${minutes}:${seconds}]`;
+});
 
 const logger = createLogger({
   level: 'info',
@@ -34,7 +34,7 @@ const logger = createLogger({
       maxFiles: '14d',
     }),
   ],
-})
+});
 
 const error_logger = createLogger({
   level: 'error',
@@ -55,6 +55,6 @@ const error_logger = createLogger({
       maxFiles: '14d',
     }),
   ],
-})
+});
 
-export { logger, error_logger }
+export { logger, error_logger };
