0a87da7d7c421b474955aada927ba98c32839111
meraj2085 <samirmeraj60@gmail.com>
2023-06-10 12:26:10 +0600

    Adding pagination

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,9 @@ import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 import { NextFunction, Request, Response } from 'express';
 import sendResponse from '../../../shared/sendResponse';
+import { IAcademicSemester } from './academicSemester.interface';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -10,7 +13,7 @@ const createSemester = catchAsync(
     const result = await AcademicSemesterService.createSemester(
       academicSemesterData
     );
-    sendResponse(res, {
+    sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Academic semester created successfully',
@@ -20,4 +23,25 @@ const createSemester = catchAsync(
   }
 );
 
-export const AcademicSemesterController = { createSemester };
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = pick(req.query, paginationFields);
+    const result = await AcademicSemesterService.getAllSemesters(
+      paginationOptions
+    );
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Academic semesters fetched successfully',
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  }
+);
+
+export const AcademicSemesterController = {
+  createSemester,
+  getAllSemesters,
+};
--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -11,32 +11,37 @@ import {
 import ApiError from '../../../errors/ApiError';
 import httpStatus from 'http-status';
 
-const academicSemesterSchema = new Schema<IAcademicSemester>({
-  title: {
-    type: String,
-    required: true,
-    enum: AcademicSemesterTitles,
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterTitles,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: AcademicSemesterMonths,
+    },
   },
-  year: {
-    type: Number,
-    required: true,
-  },
-  code: {
-    type: String,
-    required: true,
-    enum: AcademicSemesterCodes,
-  },
-  startMonth: {
-    type: String,
-    required: true,
-    enum: AcademicSemesterMonths,
-  },
-  endMonth: {
-    type: String,
-    required: true,
-    enum: AcademicSemesterMonths,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // To avoid duplicate academic semester in the same year!
 academicSemesterSchema.pre('save', async function (next) {
