b30a912971e8ae9e57f833c13ff83e45c91387f4
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-06-24 10:42:03 +0600

    Adding password hashing

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
 import { IUser, UserModel } from './user.interface';
+import config from '../../../config';
 
-const userSchema = new Schema<IUser>(
+const UserSchema = new Schema<IUser>(
   {
     id: { type: String, required: true, unique: true },
     role: { type: String, required: true },
@@ -27,4 +30,14 @@ const userSchema = new Schema<IUser>(
   }
 );
 
-export const User = model<IUser, UserModel>('User', userSchema);
+UserSchema.pre('save', async function (next) {
+  // Password Hashing
+  const user = this
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+
+export const User = model<IUser, UserModel>('User', UserSchema);
