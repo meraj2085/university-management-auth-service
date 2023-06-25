ae6ef3902adf665a10cc996a0db5bbc4aa4f5a34
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-06-25 06:54:31 +0600

    Adding login api

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import ApiError from '../../../errors/ApiError';
+import { User } from '../user/user.model';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
+import config from '../../../config';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
+
+const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
+  const { id, password } = payload;
+  const user = new User();
+  const isUserExist = await user.isUserExist(id);
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, "User doesn't exist");
+  }
+
+  // Match password
+  if (
+    isUserExist.password &&
+    !user.isPasswordMatched(password, isUserExist?.password)
+  ) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect password');
+  }
+  
+  // Create JWT token
+  const { id: userId, role, needsPasswordChange } = isUserExist;
+  const accessToken = jwtHelpers.createToken(
+    { userId, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = jwtHelpers.createToken(
+    { userId, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needsPasswordChange,
+  };
+};
+
+export const AuthService = {
+  loginUser,
+};
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
-import { IUser, UserModel } from './user.interface';
+import { IUser, IUserMethods, UserModel } from './user.interface';
 import config from '../../../config';
 
-const UserSchema = new Schema<IUser>(
+const UserSchema = new Schema<IUser, Record<string, never>, IUserMethods>(
   {
     id: { type: String, required: true, unique: true },
     role: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: 0 },
+    needsPasswordChange: { type: Boolean, default: true },
     student: {
       type: Schema.Types.ObjectId,
       ref: 'Student',
@@ -30,9 +31,27 @@ const UserSchema = new Schema<IUser>(
   }
 );
 
+// Check if user exists
+UserSchema.methods.isUserExist = async function (
+  id: string
+): Promise<Partial<IUser> | null> {
+  return await User.findOne(
+    { id },
+    { id: 1, password: 1, role: 1, needsPasswordChange: 1 }
+  ).lean();
+};
+
+// Password Matching
+UserSchema.methods.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(givenPassword, savedPassword);
+};
+
+// Password Hashing
 UserSchema.pre('save', async function (next) {
-  // Password Hashing
-  const user = this
+  const user = this;
   user.password = await bcrypt.hash(
     user.password,
     Number(config.bcrypt_salt_rounds)
